2979766912e8064d84ca9cd529089aa0a1347ca7
XaverLoeffelholz <xaver@loeffelholz.de>
2016-05-20 16:27:43 +0200

    Rotating Stage, file structure

---
+++ Assets/Face.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Face : MonoBehaviour {
+
+    public Vertice[] vertices;
+    public Vertice center;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Vertice.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Vertice : MonoBehaviour {
+    public Face face;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
